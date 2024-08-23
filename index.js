6f76464a9bb45a6fdc1bb8815cca3f1463f58dc4
mzaouchi <mzaouchi@gmail.com>
2024-08-23 12:11:31 +0100

    Second commit

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const displayTasks=()=>{
         if(tasks[i].checked){
             newContent.setAttribute('class','done')
         }
+        var newTime = document.createElement('h4')
+        newTime.innerText = tasks[i].timeDo
+
+        var newDate = document.createElement('h4')
+        newDate.innerText = tasks[i].dateDo
 
         var newCheck = document.createElement('button')
         newCheck.innerText = 'Check'
@@ -33,8 +38,10 @@ const displayTasks=()=>{
         })
 
         newDiv.appendChild(newContent)
+        newDiv.appendChild(newDate)
+        newDiv.appendChild(newTime)
         newDiv.appendChild(newCheck)
-        newDiv.appendChild(newDelete)
+        newDiv.appendChild(newDelete)        
         tasksDiv.appendChild(newDiv)        
     }
 }
@@ -45,12 +52,13 @@ var Add = document.querySelector('#Add')
 
 Add.addEventListener('click',function(){
     var inputAdd = document.querySelector('#inputAdd')
-
+    var timeToDo = document.querySelector('#timeToDo')
+    var dateToDo = document.querySelector('#dateToDo')
     if(inputAdd.value ==""){
         alert('Ekteb ya nour')
     }else{
         reCreateMainDivTask()   
-        tasks = [...tasks,{content : inputAdd.value,checked : false, id : Math.random()}]
+        tasks = [...tasks,{content : inputAdd.value,checked : false, id : Math.random(),timeDo : timeToDo.value,dateDo : dateToDo.value}]
         displayTasks()
         inputAdd.value =""
     }
@@ -61,9 +69,24 @@ Add.addEventListener('click',function(){
 const reCreateMainDivTask=()=>{
     var inputAdd = document.querySelector('#inputAdd')
     var tasksDiv = document.querySelector('#tasks')
+    
     tasksDiv.remove()
     var newDivTasks = document.createElement('div')
     newDivTasks.setAttribute('id','tasks')
     var body = document.querySelector('body')
     body.insertBefore(newDivTasks,inputAdd)   
 }
+
+
+// // Task Nour
+// var currentDate = new Date()
+// var currentMonth = currentDate.getMonth()
+// const months = [
+//   "January", "February", "March", "April", "May", "June",
+//   "July", "August", "September", "October", "November", "December"
+// ];
+
+// console.log(months[currentMonth])
+// console.log(currentDate.getUTCDate())
+// console.log(currentDate.getFullYear())
+// console.log(`${currentDate.getUTCDate()} ${months[currentMonth]} ${currentDate.getFullYear()}`)
